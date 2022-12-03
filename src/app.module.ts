159d14aa6a1475e15c5bf3f11d5fee8e316e54ff
Mustafa Turkoz <mturkoz93@gmail.com>
2022-12-03 17:48:10 +0300

    added middleware

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,21 @@
-import { Module } from '@nestjs/common';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
 import { AdminController } from './admin/admin.controller';
 import { UsersModule } from './users/users.module';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
+import { UserMiddleware } from './common/middleware/user.middleware';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController, CatsController, AdminController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware, UserMiddleware)
+      .forRoutes('*');
+  }
+}
