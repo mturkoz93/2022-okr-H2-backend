0ce37e9c241be561584d07a7844cb2fbdea28272
Mustafa Turkoz <mturkoz93@gmail.com>
2022-12-20 17:53:22 +0300

    added global middleware

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { NextFunction } from 'express';
 import { AppModule } from './app/app.module';
+
+export function globalMiddleware(req: Request, res: Response, next: NextFunction) {
+  console.log('global Middleware')
+
+  next()
+}
 
 async function bootstrap() {
   const PORT = 3000;
-  const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.use(globalMiddleware)
+
+
+  // SWAGGER
   const config = new DocumentBuilder()
     .setTitle('2022-okr-H2')
     .setDescription('The 2022-okr-H2 API description')
@@ -22,4 +33,5 @@ async function bootstrap() {
     console.log(`Swagger is running you can visit http://localhost:${PORT}/api to see the Swagger interface.`)
   })
 }
+
 bootstrap();
